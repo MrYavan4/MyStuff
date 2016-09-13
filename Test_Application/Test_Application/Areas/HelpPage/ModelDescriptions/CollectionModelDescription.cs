ef8de0ac76409189c9cd88340575c8f08a702853
tan <tanvirkhondakar@gmail.com>
2016-09-13 09:36:32 -0400

    Added a HTML and CSS page

---
+++ Test_Application/Test_Application/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Test_Application.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Test_Application/Test_Application/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Test_Application.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
